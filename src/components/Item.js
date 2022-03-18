c987ed05bc9cf76b4cea5c45f684d323f77c64e5
Zach Lefler <leaving.valhalla@gmail.com>
2022-03-17 20:25:21 -0700

    initial commit

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from 'react';
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+
+  function handleAddClick() {
+    setInCart((inCart) => !inCart);
+  }
+
+  const cartClass = inCart ? 'in-cart' : '';
+  const buttonText = inCart ? 'Remove From Cart' : 'Add to Cart';
+
   return (
-    <li className="">
+    <li className={cartClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleAddClick} className="add">
+        {buttonText}
+      </button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,35 @@
-import React from "react";
-import Item from "./Item";
+import React, { useState } from 'react';
+import Item from './Item';
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState('All');
+
+  function handleFilterChange(event) {
+    setSelectedCategory(event.target.value);
+  }
+
+  // TypeError: filteredItems.map is not a function
+
+  // const filteredItems = () => {
+  //   if (selectedCategory === 'All') {
+  //     return items;
+  //   } else {
+  //     items.filter((item) => item.category === selectedCategory);
+  //   }
+  // };
+
+  const filteredItems = items.filter((item) => {
+    if (selectedCategory === 'All') {
+      return true;
+    } else {
+      return item.category === selectedCategory;
+    }
+  });
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange={handleFilterChange} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +37,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
